a3106ced4a67dea6eac6605b813f7bfddb0edc50
Fumiya Kubota <fumiya.kubota@glucose.jp>
2019-02-10 16:18:38 +0900

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from todo_switch.config import TestingConfig
 from todo_switch.models import db
 from sanic.websocket import WebSocketProtocol
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 
 @pytest.fixture(scope='session')
@@ -18,16 +18,25 @@ def test_cli(loop, app, test_client):
     return loop.run_until_complete(test_client(app, protocol=WebSocketProtocol))
 
 
-@pytest.fixture()
-def cleanup_db():
+@pytest.fixture(scope='session', autouse=True)
+def create_db():
     testing_config = TestingConfig()
     db_host = testing_config.DB_HOST
     db_name = testing_config.DB_NAME
     engine = create_engine(f'postgresql://{db_host}/{db_name}')
     if not database_exists(engine.url):
         create_database(engine.url)
+    yield
+    drop_database(engine.url)
+    engine.dispose()
+
+
+@pytest.fixture()
+def cleanup_db():
+    testing_config = TestingConfig()
+    db_host = testing_config.DB_HOST
+    db_name = testing_config.DB_NAME
+    engine = create_engine(f'postgresql://{db_host}/{db_name}')
     db.create_all(engine)
     yield engine
-
     db.drop_all(engine)
-    engine.dispose()
